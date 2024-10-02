db0493adb05894d786e96aac3256b8c8aeac9ff7
Ninzalo <k-artemiy@mail.ru>
2024-10-02 15:17:06 +0300

    feat(plugins/highlight-colors): add highlight-colors plugin

---
+++ lua/plugins/highlight-colors.lua
@@ -0,0 +1,12 @@
+return {
+  'brenoprata10/nvim-highlight-colors',
+  lazy = false,
+  opts = {
+    enable_hex = true,
+    enable_short_hex = false,
+    enable_rgb = true,
+    enable_var_usage = true,
+    enable_named_colors = true,
+    enable_tailwind = true,
+  },
+}
