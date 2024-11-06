8b973109247b0504b19a290537428388f790b9ce
Ninzalo <k-artemiy@mail.ru>
2024-11-06 15:28:54 +0300

    feat(languages.python): update ruff configuration

--- lua/languages/python.lua
+++ lua/languages/python.lua
@@ -17,36 +17,20 @@ M.lsp_configs = {
       },
     },
   },
-  ruff_lsp = {
+  ruff = {
     autostart = true,
     filetypes = { 'python' },
     init_options = {
       settings = {
         fixAll = true,
-        lint = {
-          enable = true,
-          args = {
-            '--ignore=S603,S110,S311,S112',
-            '--ignore=N999,TID251,ANN101,ANN102,BLE001',
-            '--ignore=D107,D100,D104',
-            '--select=A,B,D,E,F,I,N,Q,R,S,W,UP,C4,C90,ANN,BLE,FBT,ASYNC,COM',
-            '--select=PIE,TID,ARG,PTH,PL,EM',
-            '--line-length=79',
-          },
-        },
-        format = {
-          args = {
-            '--line-length=79',
-            '--no-cache',
-          },
-        },
+        configuration = '~/.config/nvim/.ruff.toml',
       },
     },
   },
 }
 
 M.on_attach = {
-  ruff_lsp = {
+  ruff = {
     server_capabilities = {
       hoverProvider = false,
     },
